f11ef1ca10d5f6e11d435442ccfd34f0adc92d8f
EC2 Default User <ec2-user@ip-172-31-35-234.ap-northeast-1.compute.internal>
2025-09-10 18:52:49 +0000

    [ADD]seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "seedの実行を開始" 
+
+kitsunesuky = User.find_or_create_by!(email: "kitsune@example.com") do |user|
+  user.name = "きつねすきー"
+  user.password = "password"
+end
+
+hiyokokanteishi = User.find_or_create_by!(email: "hiyoko@example.com") do |user|
+  user.name = "ひよこ鑑定士"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/piyo.jpg"), filename:"piyo.jpg")
+end
+
+mikesan = User.find_or_create_by!(email: "mike@example.com") do |user|
+  user.name = "三毛さん"
+  user.password = "password"
+  user.profile = "3歳の三毛猫と暮らしてます。卵NG"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/mikeneko.jpg"), filename:"mikeneko.jpg")
+end
+
+puts "seedの実行が完了しました"

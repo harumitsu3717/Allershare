8dccc0c45a3340c805845ee3bee8169a5fe48fdb
EC2 Default User <ec2-user@ip-172-31-35-234.ap-northeast-1.compute.internal>
2025-10-03 19:20:15 +0000

    [Finish]add-search

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ApplicationRecord
 
   validates :body, presence: true
 
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Post.where(body: content)
+    elsif method == 'forward'
+      Post.where('body LIKE ?', content+'%')
+    elsif method == 'backward'
+      Post.where('body LIKE ?', '%'+content)
+    elsif method == 'partical'
+      Post.where('body LIKE ?', '%'+content+'%')
+    end
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions:'admin/sessions'
   }
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
     get 'mypage' => 'users#mypage', as: 'mypage'
     get 'unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
     patch 'withdraw' => 'users#withdraw', as: 'withdraw'
+    get '/search' => 'searches#search', as: 'search'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

43d6726788ac0210e7c34c0dcde411ea2830bc4e
EC2 Default User <ec2-user@ip-172-31-35-234.ap-northeast-1.compute.internal>
2026-01-07 16:51:23 +0000

    [Fix]admin_InfiniteScroll

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all.page(params[:page]).per(5)
+    @users = User.all.page(params[:page]).per(7)
   end
 
 end
